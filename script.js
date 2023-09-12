d69a2b0a369e1293615a159d859880831b07bf71
m5rius <marius585mv@gmail.com>
2023-09-12 15:24:44 +0300

    task 7 added

--- script.js
+++ script.js
@@ -1,61 +1,122 @@
 // Sukurti funkcijas, kurios paleidžia ciklą su skaičiais nuo 1 iki 100. Šie ciklai:
 // 1. Padaugina skaičių iš 2.
 
-function task1() {
-    for (let i = 1; i <= 100; i++) {
+// function task1() {
+//     for (let i = 1; i <= 100; i++) {
+//       let output = i*2
+//       console.log(output)
+//     } 
+//   }
+//   task1()
+
+// // // 2. Padaugina skaičių iš 5.
+
+// function task2() {
+//     for (let i = 1; i <= 100; i++) {
+//       let output = i*5
+//       console.log(output)
+//     } 
+//   }
+//   task2()
+
+// // // 3. Prideda prie skaičiaus 5.
+
+// function task3() {
+//     for (let i = 1; i <= 100; i++) {
+//       let output = i+5
+//       console.log(output)
+//     } 
+//   }
+//   task3()
+
+// // // 4. Atima iš skaičiaus 2.
+
+// function task4() {
+//     for (let i = 1; i <= 100; i++) {
+//       let output = i-2
+//       console.log(output)
+//     } 
+//   }
+//   task4()
+
+// // // 5. Pakelia skaičių kvadratu.
+
+// function task5() {
+//     for (let i = 1; i <= 100; i++) {
+//       let output = i*i
+//       console.log(output)
+//     } 
+//   }
+//   task5()
+
+// // // 6. Pakelia skaičių kūbu.
+
+// function task6() {
+//     for (let i = 1; i <= 100; i++) {
+//       let output = Math.pow(i,3)
+//       console.log(output)
+//     } 
+//   }
+//   task6()
+
+  //7. Sukurti analogiškas funkcijas pirmoms užduotims, tačiau padaryti, jog ciklai skaičiuotųsi nuo 100 iki 1.
+
+  // 1. Padaugina skaičių iš 2.
+
+  function task1() {
+    for (let i = 100; i >= 1; i--) {
       let output = i*2
       console.log(output)
     } 
   }
   task1()
 
-// // 2. Padaugina skaičių iš 5.
+// 2. Padaugina skaičių iš 5.
 
 function task2() {
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 100; i >= 1; i--) {
       let output = i*5
       console.log(output)
     } 
   }
   task2()
 
-// // 3. Prideda prie skaičiaus 5.
+// 3. Prideda prie skaičiaus 5.
 
 function task3() {
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 100; i >= 1; i--) {
       let output = i+5
       console.log(output)
     } 
   }
   task3()
 
-// // 4. Atima iš skaičiaus 2.
+// 4. Atima iš skaičiaus 2.
 
 function task4() {
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 100; i >= 1; i--) {
       let output = i-2
       console.log(output)
     } 
   }
   task4()
 
-// // 5. Pakelia skaičių kvadratu.
+// 5. Pakelia skaičių kvadratu.
 
-function task5() {
-    for (let i = 1; i <= 100; i++) {
+  function task5() {
+    for (let i = 100; i >= 1; i--) {
       let output = i*i
       console.log(output)
     } 
   }
   task5()
 
-// // 6. Pakelia skaičių kūbu.
+// 6. Pakelia skaičių kūbu.
 
-function task6() {
-    for (let i = 1; i <= 100; i++) {
+  function task6() {
+    for (let i = 100; i >= 1; i--) {
       let output = Math.pow(i,3)
       console.log(output)
     } 
   }
   task6()
-
